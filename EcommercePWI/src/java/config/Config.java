52553573e942e0bb9e35f6e3301b3737107ea7e8
caiofov <cfoviana@gmail.com>
2023-11-14 15:54:16 -0300

    ajuste no config

--- EcommercePWI/src/java/config/Config.java
+++ EcommercePWI/src/java/config/Config.java
@@ -4,9 +4,15 @@ package config;
  * Classe que encapsula as configurações da aplicação
  */
 public class Config {
+    //MYSQL
+    // public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    // public static final String JDBC_URL = "jdbc:mysql://localhost:3306/projetopw";
+    // public static final String JDBC_USUARIO = "root";
+    // public static final String JDBC_SENHA = "123456789";
 
-    public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    public static final String JDBC_URL = "jdbc:mysql://localhost:3306/projetopw";
-    public static final String JDBC_USUARIO = "root";
-    public static final String JDBC_SENHA = "123456789";
+    //POSTGRES
+    public static final String JDBC_DRIVER = "org.postgresql.Driver";
+    public static final String JDBC_URL = "jdbc:postgresql://localhost:5432/ecommerce";
+    public static final String JDBC_USUARIO = "postgres";
+    public static final String JDBC_SENHA = "123";
 }
