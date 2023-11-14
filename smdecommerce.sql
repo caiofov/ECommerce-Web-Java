c95eb231bb8ef9ba29524f267a4c6179259f3785
caiofov <cfoviana@gmail.com>
2023-11-14 16:29:36 -0300

    atualizar banco

--- smdecommerce.sql
+++ smdecommerce.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 12.16 (Ubuntu 12.16-0ubuntu0.20.04.1)
 -- Dumped by pg_dump version 12.16 (Ubuntu 12.16-0ubuntu0.20.04.1)
 
--- Started on 2023-10-20 13:23:24 -03
+-- Started on 2023-10-27 16:48:53 -03
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -23,6 +23,44 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- TOC entry 207 (class 1259 OID 99431)
+-- Name: categoria; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.categoria (
+    id integer NOT NULL,
+    nome character varying NOT NULL
+);
+
+
+ALTER TABLE public.categoria OWNER TO postgres;
+
+--
+-- TOC entry 206 (class 1259 OID 99429)
+-- Name: categoria_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.categoria_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.categoria_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 2998 (class 0 OID 0)
+-- Dependencies: 206
+-- Name: categoria_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.categoria_id_seq OWNED BY public.categoria.id;
+
+
+--
 -- TOC entry 205 (class 1259 OID 90486)
 -- Name: produto; Type: TABLE; Schema: public; Owner: postgres
 --
@@ -32,7 +70,8 @@ CREATE TABLE public.produto (
     descricao character varying NOT NULL,
     preco numeric NOT NULL,
     foto character varying,
-    quantidade integer NOT NULL
+    quantidade integer NOT NULL,
+    categoria_id integer NOT NULL
 );
 
 
@@ -55,7 +94,7 @@ CREATE SEQUENCE public.produto_id_seq
 ALTER TABLE public.produto_id_seq OWNER TO postgres;
 
 --
--- TOC entry 2985 (class 0 OID 0)
+-- TOC entry 2999 (class 0 OID 0)
 -- Dependencies: 204
 -- Name: produto_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -98,7 +137,7 @@ CREATE SEQUENCE public.usuario_id_seq
 ALTER TABLE public.usuario_id_seq OWNER TO postgres;
 
 --
--- TOC entry 2986 (class 0 OID 0)
+-- TOC entry 3000 (class 0 OID 0)
 -- Dependencies: 202
 -- Name: usuario_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -107,7 +146,15 @@ ALTER SEQUENCE public.usuario_id_seq OWNED BY public.usuario.id;
 
 
 --
--- TOC entry 2843 (class 2604 OID 90489)
+-- TOC entry 2851 (class 2604 OID 99434)
+-- Name: categoria id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.categoria ALTER COLUMN id SET DEFAULT nextval('public.categoria_id_seq'::regclass);
+
+
+--
+-- TOC entry 2850 (class 2604 OID 90489)
 -- Name: produto id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -115,7 +162,7 @@ ALTER TABLE ONLY public.produto ALTER COLUMN id SET DEFAULT nextval('public.prod
 
 
 --
--- TOC entry 2842 (class 2604 OID 90476)
+-- TOC entry 2849 (class 2604 OID 90476)
 -- Name: usuario id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -123,27 +170,46 @@ ALTER TABLE ONLY public.usuario ALTER COLUMN id SET DEFAULT nextval('public.usua
 
 
 --
--- TOC entry 2979 (class 0 OID 90486)
+-- TOC entry 2992 (class 0 OID 99431)
+-- Dependencies: 207
+-- Data for Name: categoria; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.categoria (id, nome) VALUES (1, 'Categoria A');
+INSERT INTO public.categoria (id, nome) VALUES (3, 'Categoria C');
+
+
+--
+-- TOC entry 2990 (class 0 OID 90486)
 -- Dependencies: 205
 -- Data for Name: produto; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.produto VALUES (1, 'PLAYSTATION 3', 990.9, NULL, 5);
-INSERT INTO public.produto VALUES (2, 'PLAYSTATION 4', 2990.9, NULL, 3);
+INSERT INTO public.produto (id, descricao, preco, foto, quantidade, categoria_id) VALUES (1, 'PLAYSTATION 3', 990.9, NULL, 5, 1);
+INSERT INTO public.produto (id, descricao, preco, foto, quantidade, categoria_id) VALUES (2, 'PLAYSTATION 4', 2990.9, NULL, 3, 1);
 
 
 --
--- TOC entry 2977 (class 0 OID 90473)
+-- TOC entry 2988 (class 0 OID 90473)
 -- Dependencies: 203
 -- Data for Name: usuario; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.usuario VALUES (1, 'Leo', 'Rua X', 'leo@ufc.br', 'leo', 'ufc123', false);
-INSERT INTO public.usuario VALUES (2, 'maria sofia', 'rua x', 'mariasofia@gmail.com', 'maria', '12345', false);
+INSERT INTO public.usuario (id, nome, endereco, email, login, senha, administrador) VALUES (1, 'Leo', 'Rua X', 'leo@ufc.br', 'leo', 'ufc123', false);
+INSERT INTO public.usuario (id, nome, endereco, email, login, senha, administrador) VALUES (2, 'maria sofia', 'rua x', 'mariasofia@gmail.com', 'maria', '12345', true);
 
 
 --
--- TOC entry 2987 (class 0 OID 0)
+-- TOC entry 3001 (class 0 OID 0)
+-- Dependencies: 206
+-- Name: categoria_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.categoria_id_seq', 4, true);
+
+
+--
+-- TOC entry 3002 (class 0 OID 0)
 -- Dependencies: 204
 -- Name: produto_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
@@ -152,7 +218,7 @@ SELECT pg_catalog.setval('public.produto_id_seq', 2, true);
 
 
 --
--- TOC entry 2988 (class 0 OID 0)
+-- TOC entry 3003 (class 0 OID 0)
 -- Dependencies: 202
 -- Name: usuario_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
@@ -161,7 +227,16 @@ SELECT pg_catalog.setval('public.usuario_id_seq', 2, true);
 
 
 --
--- TOC entry 2849 (class 2606 OID 90494)
+-- TOC entry 2859 (class 2606 OID 99439)
+-- Name: categoria categoria_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.categoria
+    ADD CONSTRAINT categoria_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2857 (class 2606 OID 90494)
 -- Name: produto produto_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -170,7 +245,7 @@ ALTER TABLE ONLY public.produto
 
 
 --
--- TOC entry 2845 (class 2606 OID 90483)
+-- TOC entry 2853 (class 2606 OID 90483)
 -- Name: usuario usuario_login_ukey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -179,7 +254,7 @@ ALTER TABLE ONLY public.usuario
 
 
 --
--- TOC entry 2847 (class 2606 OID 90481)
+-- TOC entry 2855 (class 2606 OID 90481)
 -- Name: usuario usuario_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -187,7 +262,16 @@ ALTER TABLE ONLY public.usuario
     ADD CONSTRAINT usuario_pkey PRIMARY KEY (id);
 
 
--- Completed on 2023-10-20 13:23:24 -03
+--
+-- TOC entry 2860 (class 2606 OID 99440)
+-- Name: produto produto_categoria_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.produto
+    ADD CONSTRAINT produto_categoria_id_fkey FOREIGN KEY (categoria_id) REFERENCES public.categoria(id) ON UPDATE CASCADE ON DELETE RESTRICT NOT VALID;
+
+
+-- Completed on 2023-10-27 16:48:53 -03
 
 --
 -- PostgreSQL database dump complete
