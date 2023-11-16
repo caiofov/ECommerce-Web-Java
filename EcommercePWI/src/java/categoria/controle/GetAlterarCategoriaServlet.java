276d68f79105614ab148a72bbc5062eee48b116a
caiofov <cfoviana@gmail.com>
2023-11-16 20:15:23 -0300

    get alterar categoria servlet

---
+++ EcommercePWI/src/java/categoria/controle/GetAlterarCategoriaServlet.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package categoria.controle;
+
+import categoria.modelo.CategoriaDAO;
+import categoria.modelo.Categoria;
+import produto.modelo.ProdutoDAO;
+import produto.modelo.Produto;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GetAlterarCategoriaServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int id = Integer.valueOf(request.getParameter("id"));
+        CategoriaDAO categoriaDAO = new CategoriaDAO();
+        Categoria categoria = categoriaDAO.get(id);
+
+        request.setAttribute("categoria", categoria);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/categoria/alterar.jsp");
+        dispatcher.forward(request, response);
+    }
+}
