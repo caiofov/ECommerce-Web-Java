23422e5e0a18c790a58f00c384f01fcc2fb5382e
caiofov <cfoviana@gmail.com>
2023-11-28 18:40:06 -0300

    codigos do carrinho

---
+++ EcommercePWI/src/java/carrinhocompras/modelo/CarrinhoCompraItem.java
@@ -0,0 +1,26 @@
+package carrinhocompras.modelo;
+
+import produto.modelo.Produto;
+
+public class CarrinhoCompraItem {
+
+    private Produto produto;
+    private int quantidade;
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+}
