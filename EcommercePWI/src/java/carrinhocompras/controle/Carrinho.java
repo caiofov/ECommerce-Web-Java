068134318eda7ecfe6139f0ff936921b110acec5
caiofov <cfoviana@gmail.com>
2023-11-29 21:16:39 -0300

    servlet para o carrinho

---
+++ EcommercePWI/src/java/carrinhocompras/controle/Carrinho.java
@@ -0,0 +1,36 @@
+package carrinhocompras.controle;
+
+import carrinhocompras.modelo.CarrinhoCompras;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Carrinho extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Cookie cookie = null;
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+                if (c.getName().equals("smdecommerce.carrinho")) {
+                    cookie = c;
+                    break;
+                }
+            }
+        }
+        if (cookie == null) {
+            cookie = new Cookie("smdecommerce.carrinho", "");
+        } 
+        response.addCookie(cookie);
+        request.setAttribute("produtos", cookie.getValue());
+        RequestDispatcher dispatcher = request.getRequestDispatcher("carrinho.jsp");
+        dispatcher.forward(request, response);
+    }
+
+}
